ae1df461ee0b705d7a42e2e5e5b6cfdde9e29f9e
Jean-Michel Bodart <jbodart@argenx.com>
2022-09-25 16:21:54 +0200

    annotate using-ts.ts file

---
+++ Section 01/adjusted-project/using-ts.ts
@@ -0,0 +1,18 @@
+const button = document.querySelector("button");
+const input1 = document.getElementById("num1")! as HTMLInputElement;  // TS Type casting ensures
+const input2 = document.getElementById("num2")! as HTMLInputElement;  // the type of returned element
+
+function add(num1: number  // type: number
+            , num2: number
+            ) {
+  return num1 + num2;
+}
+
+button.addEventListener("click", function() {
+  console.log(add(  +input1.value  // prepend '+' to convert strings to numbers
+                  , +input2.value
+                  ));
+});
+
+// To compile this .ts file to .js, use the TS Compiler in terminal:
+//     tsc using.ts
