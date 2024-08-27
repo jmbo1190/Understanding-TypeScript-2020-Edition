9462e16d46a3b72f7215b81c40f9e8066cee0189
Jean-Michel Bodart <jmbodart@skynet.be>
2024-08-27 13:50:10 +0200

    Update app.ts
    Define and instantiate ProjectInput class to render a form from template 'project-input'

--- Section 09/prj-00-initial-starting-setup/src/app.ts
+++ Section 09/prj-00-initial-starting-setup/src/app.ts
@@ -1,1 +1,23 @@
-// Code goes here!
+class ProjectInput {
+
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    formElement: HTMLFormElement;
+
+    constructor() {
+        this.templateElement = document.getElementById("project-input") as HTMLTemplateElement;
+        this.hostElement = document.getElementById("app") as HTMLDivElement;
+        console.log('templateElement:', this.templateElement);
+        const importedNode = document.importNode(this.templateElement.content, true) as DocumentFragment;
+        console.log('importedNode:', importedNode);
+        this.formElement = importedNode.firstElementChild as HTMLFormElement;
+        console.log('formElement:', this.formElement);
+        this.render();
+    }
+
+    render(){
+        this.hostElement.insertAdjacentElement("afterbegin", this.formElement) as HTMLFormElement;
+    }
+}
+
+const prjInput = new ProjectInput();
