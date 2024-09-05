ab0f1e2f35ba24f3bb68342b446c3404b6b3759f
Jean-Michel Bodart <jmbodart@skynet.be>
2024-09-05 07:28:46 +0200

    Update app.ts
    Add /todos routes
    Add error-handling middleware

--- Section 15/node-app/src/app.ts
+++ Section 15/node-app/src/app.ts
@@ -1,5 +1,14 @@
 import express from "express";
+import todosRouter from "./routes/todos-routes";
 
 const app = express();
+
+// Todos Routes
+app.use("/todos", todosRouter);
+
+// Error-handling middleware
+app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
+    res.status(500).json({message: err.message});
+});
 
 app.listen(3000);
